c3a02ac66ac56defc01f05d44e0873b781a6cdea
LunaticBaka9 <347254238@qq.com>
2025-11-12 23:09:39 +0800

    maid-1

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HeaderMenu from "./components/HeaderMenu.vue";
+</script>
+
+<template>
+    <HeaderMenu />
+</template>
+
+<style scoped></style>
---
+++ src/components/Container.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderMenu from "./HeaderMenu.vue";
+</script>
+
+<template>
+    <div class="common-layout">
+        <el-container>
+            <el-header>
+                <div>
+                    <HeaderMenu /></div
+            ></el-header>
+            <el-main> <div>主要内容板块</div></el-main>
+            <el-footer>dddd</el-footer>
+        </el-container>
+    </div>
+</template>
