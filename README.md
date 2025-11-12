61382fa71596a989d787983687321d96aa0845b2
LunaticBaka9 <347254238@qq.com>
2025-11-12 23:31:53 +0800

    maid-2

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Vue 3 + Vite
+# 女仆咖啡厅 WIKI
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+毕设
 
-Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+反正就是写（
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    optimizeDeps: {
+        include: ["element-plus"],
+    },
+    server: {
+        open: true,
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+    build: {
+        sourcemap: false,
+        outDir: "./web/",
+    },
 });
